f894bbad4f77d3aac7b562d95939798604c37401
Daniella Campos <niellaca22@gmail.com>
2025-05-16 14:59:26 -0600

    mi primer commit

---
+++ 14 de mayo.py
@@ -0,0 +1,8 @@
+password = input(int(('Ingresa contrasena: ')))
+for i in a range(len(password))
+if password > max:
+    print('Contrasena no segura')
+elif password <= max:
+    print('Contrasena segura')
+
+

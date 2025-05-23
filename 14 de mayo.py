dbc26ee4b410e104151c32c731ccee3883084d96
DaniellaCampos <niellaca22@gmail.com>
2025-05-22 20:46:02 -0600

    Delete 14 de mayo.py
    because this code is really bad and the other I go to put here .....well maybe for that future we can really say "hey you finally are a cience computer engineer" or I don't know it's only a stupid dream .

--- 14 de mayo.py
+++
@@ -1,8 +0,0 @@
-password = input(int(('Ingresa contrasena: ')))
-for i in a range(len(password))
-if password > max:
-    print('Contrasena no segura')
-elif password <= max:
-    print('Contrasena segura')
-
-
